e5995f5201e7f73439f69ff0f532a9a98e88568a
Ranjeet Kumar Raj <121329638+RanjeetRaj444@users.noreply.github.com>
2023-07-16 13:53:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const router = require("./routes/user.routes");
 const router2 = require("./routes/post.routes");
 const cors = require("cors");
-app.use(cors);
+
 require("dotenv").config();
 
 const app = express();
-
+app.use(cors);
 app.use(express.json());
 
 app.use("/user", router);
